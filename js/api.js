650bbe216a10e94771813444479918b33b19a61e
rhysdoherty <rhys.s.doherty@gmail.com>
2020-03-07 16:10:08 +0000

    increased the zoom slightly

--- js/api.js
+++ js/api.js
@@ -1,7 +1,7 @@
 function initMap() {
     var map = new google.maps.Map(document.getElementById('map'), {
       center: {lat: 52.205276, lng: 0.119167},
-      zoom: 10
+      zoom: 13
     });
     
   infoWindow = new google.maps.InfoWindow;
