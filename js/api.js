913bae1e38b2ee86bd4dc4e21af128a7775556b8
rhysdoherty <rhys.s.doherty@gmail.com>
2020-03-08 22:24:55 +0000

    Added first event listener onto radius button to change the radius to 4000m

--- js/api.js
+++ js/api.js
@@ -1,71 +1,77 @@
 function initMap() {
+
     var map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: 52.205276, lng: 0.119167},
-      zoom: 13,
-      disableDefaultUI: true
+        center: { lat: 52.205276, lng: 0.119167 },
+        zoom: 13,
+        disableDefaultUI: true
     });
+
+    var circle = null;
 
     var trafficLayer = new google.maps.TrafficLayer();
     trafficLayer.setMap(null);
 
 
-    document.getElementById("traffic").addEventListener("click", function(){
+    document.getElementById("traffic").addEventListener("click", function () {
         if (trafficLayer.getMap() == null) {
             //traffic layer is disabled.. enable it
             trafficLayer.setMap(map);
-          } else {
+        } else {
             //traffic layer is enabled.. disable it
             trafficLayer.setMap(null);
-          }
-      });
+        }
+    });
 
-  infoWindow = new google.maps.InfoWindow;
+    document.getElementById("radius").addEventListener("click", function () {
+        circle.setRadius(4000);    
+    });
 
-        // Try HTML5 geolocation.
-        if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(function(position) {
+    infoWindow = new google.maps.InfoWindow;
+
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
             var pos = {
-              lat: position.coords.latitude,
-              lng: position.coords.longitude
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
             };
 
             var marker = new google.maps.Marker({
                 position: pos,
                 map: map,
                 title: 'You Are Here!'
-              });
-        
-              var circle = new google.maps.Circle({
+            });
+
+            circle = new google.maps.Circle({
                 strokeColor: '#0000d8',
                 fillColor: '#0000d8',
                 fillOpacity: 0.1,
                 map: map,
                 center: pos,
                 radius: 2000
-              });
+            });
 
             infoWindow.setPosition(pos);
             infoWindow.setContent('Location found.');
             infoWindow.open(map);
             map.setCenter(pos);
-          }, function() {
+        }, function () {
             handleLocationError(true, infoWindow, map.getCenter());
-          });
+        });
 
-        } else {
-          // Browser doesn't support Geolocation
-          handleLocationError(false, infoWindow, map.getCenter());
-        }
-      }
+    } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+    }
+}
 
-      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-        infoWindow.setPosition(pos);
-        infoWindow.setContent(browserHasGeolocation ?
-                              'Error: The Geolocation service failed.' :
-                              'Error: Your browser doesn\'t support geolocation.');
-        infoWindow.open(map);
-      }
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+        'Error: The Geolocation service failed.' :
+        'Error: Your browser doesn\'t support geolocation.');
+    infoWindow.open(map);
+}
 
-      
 
-    
+
